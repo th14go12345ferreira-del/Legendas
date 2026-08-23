076d3fecd1f9645ba5d726653c244638896204d9
th14go12345ferreira-del <th14go12345ferreira@gmail.com>
2026-08-23 11:30:21 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,14 @@ android {
     }
 
     ndkVersion = "25.2.9519653"
+    compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
 
+kotlinOptions {
+    jvmTarget = "17"
+}
     externalNativeBuild {
         cmake {
             path = file("src/main/cpp/CMakeLists.txt")
